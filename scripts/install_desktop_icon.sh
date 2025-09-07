b7235855aec3ec4c097d0fda60054c938ed3e3a2
John Ostrowick <41532579+JohnOstrowick@users.noreply.github.com>
2025-09-07 10:56:03 +0200

    added desktop icon and installer script
    added desktop icon and installer script

--- scripts/install_desktop_icon.sh
+++ scripts/install_desktop_icon.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 cp templates/colfm.desktop ~/.local/share/applications/colfm.desktop
+cp templates/colfm_desktop.desktop ~/.local/share/applications/colfm_desktop.desktop
+sudo ln -s /home/john/Github/_linux/colfm/colfm /usr/local/bin/colfm
+sudo ln -s /home/john/Github/_linux/colfm/colfm_desktop /usr/local/bin/colfm_desktop
